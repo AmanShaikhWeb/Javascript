f2c4d97e401948785fcc002c4894b8a8d2b30ba0
AMAN SHAIKH <134679991+AmanShaikhWeb@users.noreply.github.com>
2023-12-12 08:47:36 +0000

    datatype

---
+++ 01_Basics/2_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat code as newer version
+
+//alert(3+3) //this wont work because we are using node js not browser
+
+// number = 2 to power of 53 is the limitfor number
+// bigint = lowest possibility to use
+//string = '' ""
+//boolean = true/false
+//null = standalone value
+//undefined = 
+//symbol = unique
+
+// object
+
+console.log(typeof true);
+console.log(typeof "aman");
