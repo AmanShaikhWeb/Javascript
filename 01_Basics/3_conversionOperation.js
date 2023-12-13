a28317d6beba9d87b0931e6f9958881906eaeb22
AMAN SHAIKH <134679991+AmanShaikhWeb@users.noreply.github.com>
2023-12-13 07:30:50 +0000

    converstion operations

---
+++ 01_Basics/3_conversionOperation.js
@@ -0,0 +1,46 @@
+let score = 100;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "100" = 100
+// "123abc" = NaN
+// true = 1 , false = 0
+
+let isLoggedIn = "Aman"
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 = true , 0 = false
+// "" = false
+// "aman" = true
+
+let someNumber = 100
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// operation ====>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+let value = 10
+let negativeValue = -value
+console.log(negativeValue);
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**3)
+//console.log(2/2)
+//console.log(2%2)
+
+let str1 = "This"
+let str2 = " Is"
+let str3 = " Javascript"
+
+let str = str1 + str2 + str3;
+
+console.log(str);
