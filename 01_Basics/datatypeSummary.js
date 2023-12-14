e7fbddacb23cfc274ad425ed9a7dd8ddcadac923
AMAN SHAIKH <134679991+AmanShaikhWeb@users.noreply.github.com>
2023-12-14 08:59:40 +0000

    primitive and non primitive

---
+++ 01_Basics/datatypeSummary.js
@@ -0,0 +1,38 @@
+// primitive
+
+// (7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt)
+
+const value = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const temperature =null 
+let useId;
+let userEmail;
+
+let id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber = 187941652165165165165165n
+
+// non primitive (reference)
+//Array Objects Functions
+
+const heroes = ["Iron-", "Captain-America", "SpiderMan"];
+let myObject = {
+    name : "Aman",
+    age : 21,
+}
+
+const myFunction = function(){
+    console.log("helloooo");
+}
+
+console.log(typeof myFunction);
+
+
+
+
+
+
+//non primitive
