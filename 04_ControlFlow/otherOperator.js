22ac599306d91a89dceaed5ed344b2e5d8eb26c2
AMAN SHAIKH <134679991+AmanShaikhWeb@users.noreply.github.com>
2023-12-18 08:33:19 +0000

    ?? operator

---
+++ 04_ControlFlow/otherOperator.js
@@ -0,0 +1,10 @@
+// nullish coalescing operator (??) : null undefined
+
+let value1;
+value1 = 5??10
+value1 = null??10
+value1 = undefined ?? 15
+value1 = null ?? 10 ?? 20
+
+console.log(value1);
+
