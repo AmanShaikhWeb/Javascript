367a4608d1c6738ca384b62d661c65ffa24f8785
AMAN SHAIKH <134679991+AmanShaikhWeb@users.noreply.github.com>
2023-12-19 07:22:06 +0000

    while and do while loops

---
+++ 05_Loops/While_loops.js
@@ -0,0 +1,24 @@
+// while loop
+
+let index = 0;
+while (index <= 100) {
+    console.log(`The Total Number is ${index}`);
+    index++;
+}
+
+let superArray = ["IronMan", "Captain-America", "Hulk", "Spider-Man","Thor"]
+let arr = 0
+while (arr < superArray.length) {
+    console.log(`superhero is ${superArray[arr]}`);
+    arr++
+}
+
+// do while loop
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 264);
+
