53de927e8ca102a932d86aab3759a476ac99efb0
AMAN SHAIKH <134679991+AmanShaikhWeb@users.noreply.github.com>
2024-01-01 08:55:24 +0000

    using fetch api

--- 08_AdvanceJs/promises.js
+++ 08_AdvanceJs/promises.js
@@ -86,3 +86,19 @@ async function consumePromise(){
 }
 
 consumePromise()
+
+//
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch("https://jsonplaceholder.typicode.com/users")
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E:", error);
+//     }
+// }
+
+// getAllUsers()
+
