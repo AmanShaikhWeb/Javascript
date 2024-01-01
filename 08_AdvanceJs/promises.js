d22e1281353545f7b707c1ed8b9b053bbbeb738f
AMAN SHAIKH <134679991+AmanShaikhWeb@users.noreply.github.com>
2024-01-01 08:14:30 +0000

    second promise

--- 08_AdvanceJs/promises.js
+++ 08_AdvanceJs/promises.js
@@ -4,9 +4,21 @@ const promiseOne = new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log('Async Task Is Completed');
         resolve()
-    }, 5000)
+    }, 2000)
 })
 
 promiseOne.then(function(){
     console.log("Promise Completed");
+})
+
+// second task
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Second Async Task");
+        resolve()
+    },2000)
+    
+}).then(function(){
+    console.log("Second Async Resolved");
 })
