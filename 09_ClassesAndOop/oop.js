da74eef6c43158d1e831e7c70dd8ae6e8c34b3dc
AMAN SHAIKH <134679991+AmanShaikhWeb@users.noreply.github.com>
2024-01-02 08:06:54 +0000

    object oriented in javascript

--- 09_ClassesAndOop/oop.js
+++ 09_ClassesAndOop/oop.js
@@ -14,3 +14,23 @@ const user = {
 console.log(user.userName);
 console.log(user.getuserDetails());
 console.log(this); // returns empty {} in global scope
+
+//
+
+function userState(username, loginCount, isuserloggednin){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isuserloggednin = isuserloggednin;
+
+    this.greetings = function(){
+        console.log(`Welcome ${this.username}`);
+    }
+
+    return this  // it will work same without this line
+}
+
+const userone = new userState("AMan", 8, false); // new keyword is used to prevent overwriting and good practice
+console.log(userone);
+
+const usertwo = new userState("eme", 7, true);
+console.log(usertwo);
