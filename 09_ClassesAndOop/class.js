bcf78e4ae6f0a57dd6ba8bf0f94d736e6c2c62bb
AMAN SHAIKH <134679991+AmanShaikhWeb@users.noreply.github.com>
2024-01-03 10:26:08 +0000

    class in javascript

--- 09_ClassesAndOop/class.js
+++ 09_ClassesAndOop/class.js
@@ -1,19 +1,46 @@
 // class
 
-class User{
-    constructor(username, email, password){
-        this.username = username
+// class User{
+//     constructor(username, email, password){
+//         this.username = username
+//         this.password = password
+//         this.email = email
+//     }
+
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+
+//     changeUser(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+// const newUser = new User("Aman", "aman@sk.com", 126161)
+
+// console.log(newUser.encryptPassword());
+// console.log(newUser.changeUser());
+
+//behind the scene
+
+function User(username, email, password){
+    this.username = username
         this.password = password
         this.email = email
-    }
-
-    encryptPassword(){
-        return `${this.password}abc`
-    }
-
-    changeUser(){
-        return `${this.username.toUpperCase()}`
-    }
 }
 
-const newUser = new User("Aman", 2233, "aman@sk.com")
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+
+User.prototype.changeUser = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const newUser = new User("Aman", "aman@sk.com", 126161)
+
+console.log(newUser.encryptPassword());
+console.log(newUser.changeUser());
+
